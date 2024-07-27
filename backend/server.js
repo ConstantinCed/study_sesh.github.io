8c849393b53acbb7f35207e38956215a727f9bf8
Constantin Cedillo <117864122+ConstantinCed@users.noreply.github.com>
2024-07-27 11:35:18 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const db = require('./db');
+const questionRouter = require('./routes/questions');
+
 app.use(express.json());
+app.use('/questions', questionRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
